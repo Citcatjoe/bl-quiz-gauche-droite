70c0afb1b22ccd95d8bf9d2a410879186503b2ca
César Greppin <Citcatjoe@users.noreply.github.com>
2023-12-18 16:11:09 +0100

    hop

--- index.php
+++ index.php
@@ -5,20 +5,51 @@ ini_set('display_errors', '0');
 // Config
 require_once __DIR__ . '/vendor/autoload.php';
 
-
 $dirs  = array();
 $dirs[] = __DIR__;
 // Local _blocks or global _blocks ? not finished
-$dirs[] = __DIR__ . '/templates';
-$dirs[] = __DIR__ . '/templates/local';
+//$dirs[] = __DIR__ . '/templates';
 //$dirs[] = __DIR__ . '/../__is_embed_somewhere/php';
+// FTP
+//$dirs[] = __DIR__ . '/../../__is_embed_somewhere/php';
+
+
+// $templateFilePath1 = __DIR__ . '/templates/_header-and-share.html.twig';
+// if (file_exists($templateFilePath1)) {
+//     $dirs[] = __DIR__ . '/templates';
+// } else {
+//     $dirs[] = __DIR__ . '/../../__is_embed_somewhere/php'; 
+// }
+
+$templateFilePath1 = __DIR__ . '/templates/metas-and-scripts/_metas-and-scripts.html.twig';
+if (file_exists($templateFilePath1)) {
+    $dirs[] = __DIR__ . '/templates/metas-and-scripts';
+} else {
+    $dirs[] = __DIR__ . '/../../__is_embed_somewhere/php/metas-and-scripts'; 
+}
+
+$templateFilePath2 = __DIR__ . '/templates/header-and-share/_header-and-share.html.twig';
+if (file_exists($templateFilePath2)) {
+    $dirs[] = __DIR__ . '/templates/header-and-share';
+} else {
+    $dirs[] = __DIR__ . '/../../__is_embed_somewhere/php/header-and-share'; 
+}
+
+$templateFilePath3 = __DIR__ . '/templates/html-elements/_html-elements.html.twig';
+if (file_exists($templateFilePath3)) {
+    $dirs[] = __DIR__ . '/templates/html-elements';
+} else {
+    $dirs[] = __DIR__ . '/../../__is_embed_somewhere/php/html-elements'; 
+}
+
+
 
 
 $debug = false;
-// if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== FALSE || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== FALSE) {
-// 	$debug = true;
-// 	$dirs[] = __DIR__ . '/../web/_admin/templates';
-// }
+
+
+
+
 
 $loader = new Twig_Loader_Filesystem($dirs);
 $twig = new Twig_Environment($loader, ['debug' => $debug]);
